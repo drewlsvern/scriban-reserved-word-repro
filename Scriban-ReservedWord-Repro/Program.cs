2519af14360a8af8e176e580394e4d0a93d8d90e
drewlsvern <6773010+drewlsvern@users.noreply.github.com>
2023-11-01 21:56:57 -0500

    first commit

---
+++ Scriban-ReservedWord-Repro/Program.cs
@@ -0,0 +1,20 @@
+﻿using Scriban;
+
+namespace Scriban_ReservedWord_Repro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTemplate = Template.Parse("Hello this is a {{ headerComment }}, this is a name: {{ name }}");
+
+            // headerComment doesn't get replaced, name does ---- why?
+            var result = myTemplate.Render(new {headerComment = "header comment.", Name = "John Doe." });
+
+
+            Console.WriteLine(result);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
